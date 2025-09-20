684583598f4ab06c5a61f237d3ea32e7eb29c8f1
Ruggero Visintin <ruggero.visintin@mollie.com>
2025-09-20 12:17:20 +0200

    silly version

---
+++ apps/server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const mockoon = require('@mockoon/serverless');
+
+const mockEnv = require('./configs/demo.json');
+
+const requestHandler = new mockoon.MockoonServerless(mockEnv).requestListener();
+
+const app = express()
+
+app.use((req, res) => {
+    requestHandler(req, res);
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
