8233afc2238e18983fcfc43421a49733db435cae
Ruggero Visintin <ruggero.visintin@mollie.com>
2025-09-27 12:05:27 +0200

    chore: improve timestamp generation

--- apps/server/weather.js
+++ apps/server/weather.js
@@ -7,9 +7,22 @@ const requestHandler = new mockoon.MockoonServerless(mockEnv).requestListener();
 
 const app = express()
 
+function modifyResponseMiddleware(req, res, next) {
+    const oldSend = res.send;
+    res.send = function (data) {
+        console.info('Modifying response body...');
+        if (typeof data === 'string') {
+            data = data.replaceAll('$/now/$', Date.now().toString());
+        }
+        return oldSend.call(this, data);
+    };
+    next();
+}
+
+app.use(modifyResponseMiddleware);
 app.use((req, res) => {
     requestHandler(req, res);
-})
+});
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
